7042ad7687e733d5b511a868677261ecc9499c14
strongsongky <thdruddyd510@naver.com>
2022-10-12 11:05:51 +0900

    배포

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 function App() {
   const checkToken = !!localStorage.getItem("token");
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/wanted-pre-onboarding-frontend">
       <Routes>
         <Route
           path="/"
